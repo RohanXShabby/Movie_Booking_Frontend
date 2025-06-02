13ac9c378fdddfebac04474da44b522741816fed
Rohan Bisht <rohan20ctbisht@gmail.com>
2025-06-02 12:17:03 +0530

    feat: Update index.html title and favicon; add react-spinners for loading state in HomeLayout

--- src/components/layout/HomeLayout.jsx
+++ src/components/layout/HomeLayout.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../UI/Navbar'
 import Footer from '../UI/Footer'
+import { useNavigation } from 'react-router-dom'
+import { BeatLoader } from 'react-spinners'
 
 const HomeLayout = () => {
+    const navigate = useNavigation()
     return (
         <>
             <div className='min-h-screen flex flex-col justify-between text-dark-text bg-dark-primary '>
                 <Navbar />
-                <Outlet />
+                {navigate.state === "loading" ? <BeatLoader /> : <Outlet />}
+
                 <Footer />
             </div>
         </>
